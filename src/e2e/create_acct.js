24df146e27f2dff735e2d7eec5e71fa6a6dc098e
zack-w <zack13254@gmail.com>
2017-01-20 04:42:00 -0500

    Added spec for successful account creation

--- src/e2e/create_acct.js
+++ src/e2e/create_acct.js
@@ -1,30 +1,41 @@
 
-var urlChanged = function(url) {
+var urlChanged = function(url, checkEquals) {
   return function () {
     return browser.getCurrentUrl().then(function(actualUrl) {
-      return url == actualUrl;
+      return checkEquals ? (url == actualUrl) : (url != actualUrl);
     });
   };
 };
 
 describe( 'create account', function() {
   
-  it( 'should error with mismatching passwords', function() {
-      // navigate to the create account page
+  beforeEach( function() {
+      // navigate to create account pag
       browser.get( 'http://localhost:4200/signup' );
-    
-      // valid entries for all other fields
+      
+      // fill in valid info
       element( by.id( "email" ) ).sendKeys( "random@email.com" );
-    
-      // put in two different passwords
       element( by.id( "password") ).sendKeys( "aaa111bbb!" );
+      element( by.id( "password_confirmation" ) ).sendKeys( "aaa111bbb!" );
+  } );
+  
+  it( 'should error with mismatching passwords', function() {
+      // mismatch on confirm password
       element( by.id( "password_confirmation" ) ).sendKeys( "aaa111bbb!!" );
       
       // try to submit the form
       element( by.css( '[type="submit"]' ) ).click();
       
       // make sure the page doesn't change
-      browser.wait(urlChanged("http://localhost:4200/signup"), 1000);
+      browser.wait(urlChanged("http://localhost:4200/signup", true), 1000);
   } );
-    
+  
+  it( 'should change pages on successful signup', function() {
+      // try to submit the form
+      element( by.css( '[type="submit"]' ) ).click();
+      
+      // make sure the page changes
+      browser.wait(urlChanged("http://localhost:4200/signup", false), 1000);
+  } );
+  
 } );
