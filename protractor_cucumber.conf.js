059682ca46b8f4f9f0c47d048e07942695381067
zack-w <zack13254@gmail.com>
2017-01-23 11:08:34 -0500

    Added protractor-cucumber config. Renamed jasmine protractor config.

---
+++ protractor_cucumber.conf.js
@@ -0,0 +1,27 @@
+// Protractor configuration file, see link for more information
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+
+/*global jasmine */
+var SpecReporter = require('jasmine-spec-reporter');
+
+exports.config = {
+  allScriptsTimeout: 11000,
+  specs: [
+    './src/features/*.feature'
+  ],
+  capabilities: {
+    'browserName': 'chrome'
+  },
+  directConnect: true,
+  baseUrl: 'http://localhost:4200/',
+  framework: 'custom',
+  frameworkPath: require.resolve('protractor-cucumber-framework'),
+  useAllAngular2AppRoots: true,
+  cucumberOpts: {
+    require: './src/features/step_definitions/**/*.js',
+    tags: false,
+    format: 'pretty',
+    profile: false,
+    'no-source': true
+  }
+};
